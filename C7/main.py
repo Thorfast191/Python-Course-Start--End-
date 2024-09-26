b1aaad538740431c22cafb3068b8fe792d323407
Md. Arafat Islam <arafat.islam21@northsouth.edu>
2024-09-26 22:49:05 +0600

    nested loops, list, 2d-list

---
+++ C7/main.py
@@ -0,0 +1,16 @@
+# nested loops:
+# for x in range(4):
+#     for y in range(3):
+#         print(f"({x}, {y})")
+
+# List:
+# name = ["a", "b", "c", ]
+
+#2D list:
+# matrix = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ] #2D List Matrix
+
+# print(matrix[0][0])
