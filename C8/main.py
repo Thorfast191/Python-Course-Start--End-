381bbccd81f9a9154bc9942376e6de34891a288d
Md. Arafat Islam <arafat.islam21@northsouth.edu>
2024-09-29 22:40:10 +0600

    tuples, unpack, dictionaries.

---
+++ C8/main.py
@@ -0,0 +1,18 @@
+# #Tuple
+# tuple = (1, 2, 3, 4, 5)
+
+# #Tuple Unpacking
+# pont = (1,2,3)
+
+# x, y, z = pont
+
+# print(x)
+
+# #Dictionary.
+# customer = {
+#     1 : "nafis",
+#     "age": 22,
+#     "city": "Dhaka"
+# }
+
+# print(customer[1])
